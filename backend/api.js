1c2f2dea6486c7b1728186e32c4f48745e57e93f
Manoj Naik <manoj.naik.4work@gmail.com>
2025-02-18 20:14:08 +0530

    Add simple server with environment-based port configuration

--- backend/api.js
+++ backend/api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+<<<<<<< HEAD
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
@@ -6,4 +7,13 @@ dotenv.config();
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+=======
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config()
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
+>>>>>>> 4678ca4 (Add simple server with environment-based port configuration)
 });
